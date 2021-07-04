eb11d86f2e857a807cb07f64381d088e47207ce0
Hirzi <64255651+HirziDevs@users.noreply.github.com>
2021-07-04 12:18:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { MessageEmbed } = require('discord.js')
 const fs = require('fs');
 const client = new Client();
 const config = require('./config.json');
-require("discord-buttons")(client);
 
 fs.readdir('./events/', (err, files) => {
 	if (err) return console.error(err);
@@ -14,15 +13,4 @@ fs.readdir('./events/', (err, files) => {
 	});
 });
 
-client.commands = new Collection();
-
-fs.readdir('./commands/', (err, files) => {
-	if (err) return console.error(err);
-	files.forEach(file => {
-		if (!file.endsWith('.js')) return;
-		const props = require(`./commands/${file}`);
-		const commandName = file.split('.')[0];
-		client.commands.set(commandName, props);
-	});
-});
 client.login(config.token);
