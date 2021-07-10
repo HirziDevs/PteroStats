7d55011c844db827d1f427977369aa4d94a1f535
Hirzi <64255651+HirziDevs@users.noreply.github.com>
2021-07-10 13:05:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const fs = require('fs');
 const client = new Client();
 const config = require('./config.json');
 
+let nodelist = [
+{
+    id: "Server ID",
+    name: "**Example Server 1**",
+    nameid: "Node1"
+},
+{
+    id: "Server ID",
+    name: "**Example Server 2**",
+    nameid: "Node2"
+},
+//Add more code like bellow to the nodeslist array after comma and before "]" if you want to add more nodes
+//{
+//    id: "Server ID",
+//    name: "**Example Server 3**",
+//    nameid: "Node3"
+//},
+]
+client.nodelist = nodelist
+
 fs.readdir('./events/', (err, files) => {
 	if (err) return console.error(err);
 	files.forEach(file => {
@@ -13,4 +33,5 @@ fs.readdir('./events/', (err, files) => {
 	});
 });
 
+if(config.token === "BOT TOKEN") console.log(chalk.blue("[PteroStats Checker] ") + chalk.red("Invalid Token, Check ") + chalk.green("config.json") + chalk.red(" file to change token"))
 client.login(config.token);
