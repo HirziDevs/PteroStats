fc8d62299fc0903b81754888a7be0e330e33e450
Hirzi <64255651+HirziDevs@users.noreply.github.com>
2021-07-19 16:57:39 +0700

    v1.4.0

---
+++ nodes.js
@@ -0,0 +1,23 @@
+let nodelist = [
+    {
+        id: 'Server ID',
+        name: 'Example Server 1',
+        nameid: 'Node1',
+        nodeid: '1'
+    },
+    {
+        id: 'Server ID',
+        name: 'Example Server 2',
+        nameid: 'Node2',
+        nodeid: '2'
+    },
+    //Add more code like bellow to the nodeslist array after comma and before ']' if you want to add more nodes
+    //{
+    //    id: 'Server ID',
+    //    name: 'Example Server 3',
+    //    nameid: 'Node3',
+    //    nodeid: '1'
+    //},
+]
+
+module.exports = nodelist;
