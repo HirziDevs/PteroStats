ea610ae7ba356778cd7c506c55c28ceabcebdfac
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-09-08 19:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const { Client, Collection } = require('discord.js')
+const Discord = require('discord.js')
 const fs = require('fs')
-const client = new Client()
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+    Discord.Intents.FLAGS.GUILD_INVITES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    Discord.Intents.FLAGS.GUILD_PRESENCES,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+  allowedMentions: {
+    parse: ["everyone", "roles", "users"],
+    repliedUser: true
+  },
+  partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
+
+});
+
 const yaml = require('js-yaml')
 const chalk = require('chalk')
 const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'))
