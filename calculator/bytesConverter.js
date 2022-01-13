3dac39c9cedaec9b973e9a9edecdcbc0e6b38713
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-01-13 11:05:31 +0530

    updates

--- calculator/bytesConverter.js
+++ calculator/bytesConverter.js
@@ -1,44 +1,44 @@
 module.exports = (value, unit) => {
   if(unit == "Bit" || unit == "Bits"){
     if(value >= 8){
-      value = Math.round(value/8);
+      value = Math.round(value/8)
     }
     if(value > 1){
-      unit = "Bytes";
+      unit = "Bytes"
     }else{
-      unit = "Byte";
+      unit = "Byte"
     }
   }
   if(unit == "Byte" || unit == "Bytes"){
     if(value >= 1024){
-      value = Math.round(value/1024);
+      value = Math.round(value/1024)
       unit = "KB"
     }
   }
   if(unit == "KB"){
     if(value >= 1024){
-      value = Math.round(value/1024);
-      unit = "MB";
+      value = Math.round(value/1024)
+      unit = "MB"
     }
   }
   if(unit == "MB"){
     if(value >= 1024){
-      value = Math.round(value/1024);
-      unit = "GB";
+      value = Math.round(value/1024)
+      unit = "GB"
     } 
   }
   if(unit == "GB"){
     if(value >= 1024){
-      value = Math.round(value/1024);
-      unit = "TB";
+      value = Math.round(value/1024)
+      unit = "TB"
     }
   }
   if(unit == "TB"){
     if(value >= 1024){
-      value = Math.round(value/1024);
-      unit = "PB";
+      value = Math.round(value/1024)
+      unit = "PB"
     }
   }
 
-  return `${value.toFixed(2)}${unit}`;
+  return `${value.toFixed(2)}${unit}`
 }
--- calculator/timeConverter.js
+++ calculator/timeConverter.js
@@ -1,100 +1,100 @@
 module.exports = (value, unit) => {
-  let ms = "0ms";
-  let sec = "0sec";
-  let min = "0min";
-  let hour = "0hour";
-  let day = "0day";
-  let month = "0month";
-  let year = "0year";
+  let ms = "0ms"
+  let sec = "0sec"
+  let min = "0min"
+  let hour = "0hour"
+  let day = "0day"
+  let month = "0month"
+  let year = "0year"
   if(unit == "ms"){
     if(value >= 1000){
-      ms = `${Math.round(value%1000)}ms`;
-      value = Math.round(value/1000);
-      unit = "sec";
-      sec = `${value}sec`;
+      ms = `${Math.round(value%1000)}ms`
+      value = Math.round(value/1000)
+      unit = "sec"
+      sec = `${value}sec`
       if(value > 1){
-        sec = sec + 's';
+        sec = sec + 's'
       }
     }
   }
   if(unit == "sec" || unit == "secs"){
     if(value >= 60){
-      sec = `${Math.round(value%60)}sec`;
-      value = Math.round(value/60);
-      unit = "min";
-      min = `${value}min`;
+      sec = `${Math.round(value%60)}sec`
+      value = Math.round(value/60)
+      unit = "min"
+      min = `${value}min`
       if(value > 1){
-        min = min + 's';
+        min = min + 's'
       }
     } 
   }
   if(unit == "min" || unit == "mins"){
     if(value >= 60){
-      min = `${Math.round(value%60)}min`;
-      value = Math.round(value/60);
-      unit = "hour";
-      hour = `${value}hour`;
+      min = `${Math.round(value%60)}min`
+      value = Math.round(value/60)
+      unit = "hour"
+      hour = `${value}hour`
       if(value > 1){
-        hour = hour + 's';
+        hour = hour + 's'
       }
     } 
   }
   if(unit == "hour" || unit == "hours"){
     if(value >= 24){
-      hour = `${Math.round(value%24)}hour`;
-      value = Math.round(value/24);
-      unit = "day";
-      day = `${value}day`;
+      hour = `${Math.round(value%24)}hour`
+      value = Math.round(value/24)
+      unit = "day"
+      day = `${value}day`
       if(value > 1){
-        day = day + 's';
+        day = day + 's'
       }
     }
   }
   if(unit == "day" || unit == "days"){
     if(value >= 30){
-      day = `${Math.round(value%30)}day`;
-      value = Math.round(value/30);
-      unit = "month";
-      month = `${value}month`;
+      day = `${Math.round(value%30)}day`
+      value = Math.round(value/30)
+      unit = "month"
+      month = `${value}month`
       if(value > 1){
-        month = month + 's';
+        month = month + 's'
       }
     } 
   }
   if(unit == "month" || unit == "months"){
     if(value >= 30){
-      month = `${Math.round(value%30)}month`;
-      value = Math.round(value/30);
-      unit = "year";
-      year = `${value}year`;
+      month = `${Math.round(value%30)}month`
+      value = Math.round(value/30)
+      unit = "year"
+      year = `${value}year`
       if(value > 1){
-        year = year + 's';
+        year = year + 's'
       }
     } 
   }
 
   if(unit == "year" || unit == "years"){
-    return year + " " + month + " " + day + " " + hour + " " + min + " " + sec + " " + ms;
+    return year + " " + month + " " + day + " " + hour + " " + min + " " + sec + " " + ms
   }
   else if(unit == "month" || unit == "months"){
-    return month + " " + day + " " + hour + " " + min + " " + sec + " " + ms;
+    return month + " " + day + " " + hour + " " + min + " " + sec + " " + ms
   }
   else if(unit == "day" || unit == "days"){
-    return day + " " + hour + " " + min + " " + sec + " " + ms;
+    return day + " " + hour + " " + min + " " + sec + " " + ms
   }
   else if(unit == "hour" || unit == "hours"){
-    return hour + " " + min + " " + sec + " " + ms;
+    return hour + " " + min + " " + sec + " " + ms
   }
   else if(unit == "min" || unit == "mins"){
-    return min + " " + sec + " " + ms;
+    return min + " " + sec + " " + ms
   }
   else if(unit == "sec" || unit == "sec"){
-    return sec + " " + ms;
+    return sec + " " + ms
   }else if(unit == "ms"){
-    return ms;
+    return ms
   }
   else{
-    console.log("unknown unit");
-    return;
+    console.log("unknown unit")
+    return
   }
 }
