fe7d0efbbe844b594e7507712cb16f76d1774c96
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-01-14 08:33:21 +0530

    Update power.js

--- commands/power.js
+++ commands/power.js
@@ -34,48 +34,20 @@ module.exports = {
       let adminAccountAPIKey = client.config.adminAccountAPIKey
       let responseData = await APIFetcher(client, "client", `/servers/${args[0]}/resources/`, 1)
       let attributes = responseData.attributes
-      let state = attributes.current_state
       let isSuspended = attributes.is_suspended
-      let resources = attributes.resources
-      let cpuUsage = `${resources.cpu_absolute.toFixed(2)}`
-      let ramUsage = resources.memory_bytes/(1024*1024)
-      let diskUsage = resources.disk_bytes/(1024*1024)
-      let uptime = timeConverter(resources.uptime, "ms")
       responseData = await APIFetcher(client, "client", `/servers/${args[0]}/`, 1)
       attributes = responseData.attributes
       let name = attributes.name
       let node = attributes.node
       let uuid = attributes.uuid
       let description = attributes.description
+      let isInstalling = attributes.is_installing
       if(!description){
         description = " "
       }
       if(description.length>60){
         description.length = 57
         description = description + "..."
-      }
-      let ramTotal = attributes.limits.memory
-      let diskTotal = attributes.limits.disk
-      let cpuTotal = `${attributes.limits.cpu.toFixed(2)}`
-      let databases = attributes.feature_limits.databases
-      let ports = attributes.feature_limits.allocations
-      let backups = attributes.feature_limits.backups
-      let isInstalling = attributes.is_installing
-      let RAM, DISK, CPU
-      if(ramTotal <= 0.00){
-        RAM = `${bytesConverter(ramUsage, "MB")} Used`
-      }else{
-        RAM = `${bytesConverter(ramUsage, "MB")}/${bytesConverter(ramTotal, "MB")} [${percentageCalculator(ramUsage, ramTotal)}]`
-      }
-      if(diskTotal <= 0.00){
-        DISK = `${bytesConverter(diskUsage, "MB")} Used`
-      }else{
-        DISK = `${bytesConverter(diskUsage, "MB")}/${bytesConverter(diskTotal, "MB")} [${percentageCalculator(diskUsage, diskTotal)}]`
-      }
-      if(cpuTotal <= 0.00){
-        CPU = `${cpuUsage}% Used`
-      }else{
-        CPU = `${cpuUsage}%/${cpuTotal}%`
       }
       if(isInstalling){
         embed.setTitle("Server Stats")
