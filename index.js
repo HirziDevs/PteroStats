e5457c02bb3e0594a02a1c9b4bea7c1428969bcb
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2022-02-09 14:20:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents, Collection } = require('discord.js')
+const { Client, Intents } = require('discord.js')
 const fs = require('fs')
 const client = new Client({ 
   intents: [
