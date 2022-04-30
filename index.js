25162107c64cd72d1517fde22b8a228eb7cd60c6
Serverkur | Acktar <acktar@serverkur.net>
2022-04-30 13:48:45 +0400

    Not much but i hate editing line by line

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { Client, Collection } = require('discord.js')
-const fs = require('fs')
-const client = new Client()
-const yaml = require('js-yaml')
-const chalk = require('chalk')
-const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'))
-client.config = config
+const fs = require('fs');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const yaml = require('js-yaml');
+const chalk = require('chalk');
 
+// Load config file and bind it to the process.env so that we can access it from other files
+const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+process.env = config
+
+// Register all events files from the events folder
 fs.readdir('./events/', (err, files) => {
     if (err) return console.error(err)
     files.forEach(file => {
@@ -15,5 +18,6 @@ fs.readdir('./events/', (err, files) => {
     })
 })
 
-if (config.token === 'BOT TOKEN') console.log(chalk.blue('[PteroStats Checker] ') + chalk.red('Invalid Token, Check ') + chalk.green('config.yml') + chalk.red(' file to change token'))
-client.login(config.token)
+// Login to the bot
+if (process.env.client.token === 'Discord Bot Token') console.log(chalk.blue('[PteroStats Checker] ') + chalk.red('Please provide a bot token to run the bot on, Check ') + chalk.green('config.yml') + chalk.red(' file to change the token'))
+client.login(process.env.client.token)
