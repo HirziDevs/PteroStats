b0d32105f6457f88f855e27acf40eac0c6ddcca0
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2023-07-11 18:11:17 +0530

    New Features-Replace promises with async await, recoded and fixed bugs i could find, created seperate modules for calculation, made the pakcges intrigity check better, updated packages version

--- index.js
+++ index.js
@@ -1,75 +1,52 @@
-const fs = require('fs');
-const child = require('child_process');
+const intigrityCheck = require("./modules/intigrityCheck");
 
-function InstallPackages() {
-	console.log('You didn\'t install the required node packages first!')
-	console.log('Please wait... starting to install all required node packages using child process')
-	console.log('If the bot can\'t install the package please install it manually')
-	try {
-		child.execSync('npm i')
-		console.log('Install complete!, please run "node index" command again!')
-		process.exit()
-	} catch (err) {
-		console.log('Error! ', err)
-		console.log('Support Server: https://discord.gg/zv6maQRah3')
-		process.exit()
-	}
+if(!intigrityCheck()){
+  return console.log("Intigrity check failed!");
 }
 
-if (Number(process.version.split('.')[0]) < 16) {
-	console.log('Invalid NodeJS Version!, Please use NodeJS 16.x or upper')
-	process.exit()
-}
-if (fs.existsSync('./node_modules')) {
-	if (fs.existsSync('./node_modules/discord.js')) {
-		const check = require('./node_modules/discord.js/package.json')
-		if (Number(check.version.split('.')[0]) !== 14) {
-			console.log('Invalid Discord.JS Version!, Please use Discord.JS 14.x')
-			process.exit()
-		}
-	} else InstallPackages()
-	if (fs.existsSync('./node_modules/axios')) {
-		const check = require('./node_modules/axios/package.json')
-		if (Number(check.version.split('.')[1]) > 1) {
-			console.log('Invalid Axios Version!, Please use Axios 1.1.3')
-			process.exit()
-		}
-	} else InstallPackages()
-	if (fs.existsSync('./node_modules/chalk')) {
-		const check = require('./node_modules/chalk/package.json')
-		if (Number(check.version.split('.')[0]) > 4) {
-			console.log('Invalid Chalk Version!, Please use Chalk 4.1.2')
-			process.exit()
-		}
-	} else InstallPackages()
-	if (!fs.existsSync('./node_modules/js-yaml')) InstallPackages()
-} else InstallPackages()
-
-const chalk = require('chalk');
-const yaml = require('js-yaml');
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require("node:fs");
+const chalk = require("chalk");
+const yaml = require("js-yaml");
+const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.config = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+client.config = yaml.load(fs.readFileSync("./config.yml", "utf8"));
 
-if (client.config.panel.adminkey || client.config.resource || client.config.message.image) {
-	console.log(chalk.cyan('[PteroStats] ') + chalk.red('You are using old config file, please update your config file at ') + chalk.green('https://github.com/HirziDevs/PteroStats/blob/main/config.yml'))
-	process.exit()
-}
-if (client.config.token.startsWith('Put') || !client.config.token.length) {
-	console.log(chalk.cyan('[PteroStats] ') + chalk.red('Error! Invalid Discord Bot Token'))
-	process.exit()
+if (
+  client.config.panel.adminkey ||
+  client.config.resource ||
+  client.config.message.image
+) {
+  console.log(
+    chalk.cyan("[PteroStats] ") +
+      chalk.red(
+        "You are using old config file, please update your config file at "
+      ) +
+      chalk.green(
+        "https://github.com/HirziDevs/PteroStats/blob/main/config.yml"
+      )
+  );
+  process.exit();
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+if (client.config.token.startsWith("Put") || !client.config.token.length) {
+  console.log(
+    chalk.cyan("[PteroStats] ") + chalk.red("Error! Invalid Discord Bot Token")
+  );
+  process.exit();
+}
+
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.login(client.config.token);
---
+++ modules/intigrityCheck.js
@@ -0,0 +1,127 @@
+const fs = require("node:fs");
+const child = require("node:child_process");
+const path = require("node:path");
+
+const REQUIRED_PACKAGES = [
+  {
+    name: "axios",
+    version: "1.4.0",
+  },
+  {
+    name: "axios-retry",
+    version: "3.5.1",
+  },
+  {
+    name: "chalk",
+    version: "4.1.2",
+  },
+  {
+    name: "discord.js",
+    version: "14.11.0",
+  },
+  {
+    name: "js-yaml",
+    version: "4.1.0",
+  },
+];
+const NODE_VERSION = 16;
+
+function InstallPackages() {
+  console.log("Required nodejs packages not found!");
+  console.log("Please wait... starting to install all required node packages.");
+  console.log(
+    "If the bot can't install the packages please install them manually."
+  );
+
+  try {
+    let packagesList = "";
+
+    for (const package of REQUIRED_PACKAGES) {
+      packagesList += ` ${package.name}@${package.version}`;
+    }
+
+    child.execSync(`npm i${packagesList}`);
+    console.log('Install complete!, please run "node index" command again!');
+
+    process.exit();
+  } catch (err) {
+    console.log("Error! ", err);
+    console.log("Support Server: https://discord.gg/zv6maQRah3");
+
+    process.exit();
+  }
+}
+
+module.exports = () => {
+  if (Number(process.version.split(".")[0]) < NODE_VERSION) {
+    console.log(
+      `Unsupported NodeJS Version!, Please use NodeJS ${NODE_VERSION}.x or higher.`
+    );
+
+    process.exit();
+  }
+
+  const nodeModulesFolderPath = path.join(__dirname, "../", "node_modules");
+
+  if (fs.existsSync(nodeModulesFolderPath)) {
+    let success = false;
+    let errorMessage = "";
+
+    for (const package of REQUIRED_PACKAGES) {
+      const packageFilePath = path.join(
+        __dirname,
+        "../",
+        "node_modules",
+        package.name,
+        "package.json"
+      );
+
+      if (fs.existsSync(packageFilePath)) {
+        let packageFile = fs.readFileSync(packageFilePath, 'utf-8');
+
+        if (packageFile) {
+          packageFile = JSON.parse(packageFile);
+
+          if (
+            Number(packageFile.version.split(".")[1]) !==
+            Number(package.version.split(".")[1])
+          ) {
+            console.log(
+              `Unsupported "${package.name}" version!.\nPlease delete your "node_modules" and "package-lock.json".\nAnd restart the bot.`
+            );
+
+            process.exit();
+          } else {
+            success = true;
+
+            continue;
+          }
+        } else {
+          success = false;
+
+          errorMessage = `Unknown package version- "${package.name}".`;
+
+          break;
+        }
+      } else {
+        success = false;
+
+        errorMessage = `Missing package- "${package.name}".`;
+
+        break;
+      }
+    }
+
+    if (!success) {
+      if (errorMessage) {
+        console.log(errorMessage);
+      }
+
+      InstallPackages();
+    }
+  } else {
+    InstallPackages();
+  }
+
+  return true;
+};
