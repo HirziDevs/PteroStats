b7981dc39a426b92575239c7569d823734144567
hirzidevs <hirzidevs@gmail.com>
2024-08-25 21:35:19 +0700

    Remove Validate Function

--- handlers/config.js
+++ handlers/config.js
@@ -9,16 +9,12 @@ if (fs.existsSync("config-dev.yml")) {
     config = yaml.load(fs.readFileSync("./config-dev.yml", "utf8"));
 }
 
-validateURL(config.panel.url);
-
-function validateURL(url) {
-    try {
-        const testURL = new URL(url);
-        if (!testURL.protocol.startsWith("http")) throw new Error();
-    } catch {
-        console.error('Config Error | Invalid URL Format! Example Correct URL: "https://panel.example.com"');
-        process.exit();
-    }
+try {
+    const testURL = new URL(config.panel.url);
+    if (!testURL.protocol.startsWith("http")) throw new Error();
+} catch {
+    console.error('Config Error | Invalid URL Format! Example Correct URL: "https://panel.example.com"');
+    process.exit();
 }
 
 module.exports = config
