c6996eb80bf3fc623b8bab81f1a6237d90b1cd45
Hirzi <HirziDevs@gmail.com>
2024-09-11 08:19:00 +0700

    feat: update resource unit conversion, add "byte" and "percentage" as allowed values

--- handlers/convertUnits.js
+++ handlers/convertUnits.js
@@ -1,20 +1,15 @@
+const prettyBytes = require('prettier-bytes');
+
 module.exports = function convertUnits(value1, value2, unit) {
     unit = unit.toUpperCase();
-    const units = ['MB', 'GB', 'TB'];
-    const conversionFactors = {
-        'MB': 1,
-        'GB': 1 / 1024,
-        'TB': 1 / (1024 * 1024)
-    };
-
-    if (unit === '%' || unit === "PERCENTAGE" || unit === "PERCENT") {
-        const percentage = Math.floor((value1 / value2) * 100);
-        return `${!percentage ? 0 : percentage}%`;
-    } else if (units.includes(unit)) {
-        const formatValue = (value) => Number.isInteger(value) ? value.toLocaleString() : value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-
-        return `${formatValue(value1 * conversionFactors[unit])} ${unit}/${(value2 === 0 || value2 < 1) ? "Unlimited" : formatValue(value2 * conversionFactors[unit]) + " " + unit}`;
-    } else {
-        return `${value1.toLocaleString()} MB/${value2.toLocaleString()} MB`;
+    switch (unit) {
+        case 'PERCENTAGE':
+        case 'PERCENT':
+            const percentage = Math.floor((value1 / value2) * 100);
+            return `${!percentage ? 0 : percentage}%`;
+        case 'BYTE':
+            return `${prettyBytes(value1)} / ${prettyBytes(value2)}`;
+        default:
+            return `${value1.toLocaleString()} ${unit}/${value2.toLocaleString()} ${unit}`;
     }
 }
