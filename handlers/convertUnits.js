1f4eeae9422934b3462a271f966231fb658fa675
Hirzi <HirziDevs@gmail.com>
2024-09-11 09:50:18 +0700

    fix: correct unit conversion displaying wrong resource

--- handlers/convertUnits.js
+++ handlers/convertUnits.js
@@ -8,8 +8,8 @@ module.exports = function convertUnits(value1, value2, unit) {
             const percentage = Math.floor((value1 / value2) * 100);
             return `${!percentage ? 0 : percentage}%`;
         case 'BYTE':
-            return `${prettyBytes(value1)} / ${prettyBytes(value2)}`;
+            return `${prettyBytes(value1 * 1000000)} / ${value2 === 0 ? "Unlimited" : prettyBytes(value2 * 1000000)}`;
         default:
-            return `${value1.toLocaleString()} ${unit}/${value2.toLocaleString()} ${unit}`;
+            return `${value1.toLocaleString()} ${unit}/${value2 === 0 ? "Unlimited" : `${value2.toLocaleString()} ${unit}`}`;
     }
 }
