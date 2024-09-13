02c8dc13db022a76ab7dafa187b5c9fccc3a2522
hirzidevs <hirzidevs@gmail.com>
2024-09-13 08:44:20 +0700

    feat: update config version to 5

--- handlers/config.js
+++ handlers/config.js
@@ -18,7 +18,7 @@ try {
     process.exit();
 }
 
-if (config.version !== 5) {
+if (config.version !== 6) {
     console.error('Config Error | Invalid config version! The config has been updated, please get the new config from https://github.com/HirziDevs/PteroStats/blob/dev/config.yml');
     process.exit();
 }
